7e07c1fdee6c267e82be1d45bc7e80819ef9dd75
Jiexiao <2519639086@qq.com>
2021-05-23 16:46:37 +0800

    update

--- train_PReNet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Rain100H
-python train_PReNet.py --preprocess True --save_path /gdata/xiaojie/Prenet_model --data_path /gdata/xiaojie/Prenet_Dataset/train/RainTrainH --name Rain100H --epochs 1
-
-# Rain100L
-python train_PReNet.py --preprocess True --save_path /gdata/xiaojie/Prenet_model --data_path /gdata/xiaojie/Prenet_Dataset/train/RainTrainL --name Rain100L --resume --epochs 1
-
